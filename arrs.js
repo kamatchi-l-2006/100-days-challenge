0802d96a673dccaae3bbef359142672473080382
kamatchi-l-2006 <kamatchiloganathan16@gmail.com>
2026-03-11 20:33:39 +0530

    date-06-task
    Signed-off-by: kamatchi-l-2006 <kamatchiloganathan16@gmail.com>

--- arrs.js
+++ arrs.js
@@ -1,15 +1,15 @@
 const arr = [1, 2, 3, 4, 5]; 5
             0,1,2,3,4
-// console.log(arr.length);
-// console.log(arr[0]+2); //1+2
-// console.log(arr[1]+2);//2+2
-// console.log(arr[2]+2);//3+2
-// console.log(arr[3]+2);
-// console.log(arr[4]+2);
+console.log(arr.length);
+console.log(arr[0]+2); //1+2
+console.log(arr[1]+2);//2+2
+console.log(arr[2]+2);//3+2
+console.log(arr[3]+2);
+console.log(arr[4]+2);
 
-// for(let i=2; i<arr.length; i++){
-//     console.log(arr[i]);
-// }   
+for(let i=2; i<arr.length; i++){
+    console.log(arr[i]);
+}   
 // i=0, i<5
 // arr[0]+2 = 3
 // i=1 , i<5
