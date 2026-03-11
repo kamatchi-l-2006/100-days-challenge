626fa173efc7a07376245cabf82882134dc8a2e1
kamatchi-l-2006 <kamatchiloganathan16@gmail.com>
2026-03-11 21:12:46 +0530

    date-06-task

--- arrs2.js
+++ arrs2.js
@@ -1,12 +1,12 @@
 const arr=[1,2,3,4,5];
-// arr=[2,3]; 
-// console.log(arr);
-try{
 arr=[2,3]; 
 console.log(arr);
-}
-catch(err){
-    // console.log(err);
-}
+// try{
+// arr=[2,3]; 
+// console.log(arr);
+// }
+// catch(err){
+//     // console.log(err);
+// }
 
-console.log("hi");
+// console.log("hi");
