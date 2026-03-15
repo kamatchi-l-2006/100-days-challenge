cd848ce1726bef220cd237c95633b1c58e09a878
kamatchi-l-2006 <kamatchiloganathan16@gmail.com>
2026-03-15 21:03:38 +0530

    date-04mar-task
    Signed-off-by: kamatchi-l-2006 <kamatchiloganathan16@gmail.com>

--- arrs.js
+++ arrs.js
@@ -9,24 +9,7 @@ console.log(arr[4]+2);
 for(let i=2; i<arr.length; i++){
     console.log(arr[i]);
 }   
-// i=0, i<5
-// arr[0]+2 = 3
-// i=1 , i<5
-// arr[1] +2 = 2+2 = 4
-// i=2 , i<5
-// arr[2]+2= 3+2 = 5
-// i=3 , i<5
-// arr[3]+2 = 4+2 = 6
-// i=4 , i<5
-// arr[4]+2 = 5+2 = 7
-// i=5, i<5
 
 for(let i of arr){
     console.log(i,"th"+"element:",i);
 }
-// arr= 1,2,3,4,5
-// i=1
-// i=2
-// i=3
-// i=4
-// i=5     
