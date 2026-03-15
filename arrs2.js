fd718bf9cbe0daf64bc568575281fb5c3807da75
kamatchi-l-2006 <kamatchiloganathan16@gmail.com>
2026-03-15 21:05:10 +0530

    date-04mar-task
    Signed-off-by: kamatchi-l-2006 <kamatchiloganathan16@gmail.com>

--- arrs2.js
+++ arrs2.js
@@ -1,12 +1,3 @@
 const arr=[1,2,3,4,5,6];
 arr=[2,3]; 
 console.log(arr);
-// try{
-// arr=[2,3]; 
-// console.log(arr);
-// }
-// catch(err){
-//     // console.log(err);
-// }
-
-// console.log("hi");
