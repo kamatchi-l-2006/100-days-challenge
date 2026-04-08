04ebb455f6c45c6eaecf1b8ecdba84cb8349fe09
kamatchi-l-2006 <kamatchiloganathan16@gmail.com>
2026-04-08 21:34:41 +0530

    16-03-task
    Signed-off-by: kamatchi-l-2006 <kamatchiloganathan16@gmail.com>

--- 16-03-2026/return.js
+++ 16-03-2026/return.js
@@ -1,5 +1,5 @@
 const result = [10, 20, 30].forEach( num => num * 2);
-console.log(result)
+console.log(result);
 
 Answer: It's undefined because the `forEach` method does not 
 return a new array or any value.   
